5679ed4e78da91c8fed73ecfc4a501bc8d2238b2
Cesar Hinojosa <cesarivanhinojosa@gmail.com>
2026-06-12 07:53:56 -0500

    feat(engine): add scene

---
+++ scene.cpp
@@ -0,0 +1,18 @@
+#include "scene.hpp"
+#include "game_object.hpp"
+#include <memory>
+
+GameObject &Scene::spawn() {
+  objects.push_back(std::make_unique<GameObject>());
+  return *objects.back();
+}
+
+void Scene::update(float dt) {
+  for (auto &o : objects)
+    o->update(dt);
+}
+
+void Scene::draw() {
+  for (auto &o : objects)
+    o->draw();
+}
---
+++ scene.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "game_object.hpp"
+#include <memory>
+#include <vector>
+
+class Scene {
+public:
+  GameObject &spawn();
+  void update(float dt);
+  void draw();
+
+private:
+  std::vector<std::unique_ptr<GameObject>> objects;
+};
