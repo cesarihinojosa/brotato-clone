f66c9c07f9cd5505148f0ecac3433e3b27e9cb89
Cesar Hinojosa <cesarivanhinojosa@gmail.com>
2026-07-07 07:02:32 -0500

    feat: add scene query with view()

--- core/scene.hpp
+++ core/scene.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include "game_object.hpp"
 #include <memory>
+#include <tuple>
 #include <vector>
 
 /// Manages all GameObjects and drives the main update/draw loop.
 class Scene {
 public:
-  /// Creates a new empty GameObject, adds it to the scene, and returns a reference.
-  /// The reference remains valid for the lifetime of the Scene.
+  /// Creates a new empty GameObject, adds it to the scene, and returns a
+  /// reference. The reference remains valid for the lifetime of the Scene.
   GameObject &spawn();
 
   /// Calls update(dt) on all living GameObjects.
@@ -15,6 +16,20 @@ public:
 
   /// Calls draw() on all living GameObjects.
   void draw() const;
+
+  template <typename... Ts>
+  std::vector<std::tuple<GameObject &, Ts &...>> view() {
+    std::vector<std::tuple<GameObject &, Ts &...>> result;
+    for (auto &o : objects) {
+      if (!o->alive)
+        continue;
+      auto ptrs = std::make_tuple(o->getComponent<Ts>()...);
+      if ((std::get<Ts *>(ptrs) && ...)) {
+        result.emplace_back(*o, *std::get<Ts *>(ptrs)...);
+      }
+      return result;
+    }
+  }
 
 private:
   std::vector<std::unique_ptr<GameObject>> objects;
